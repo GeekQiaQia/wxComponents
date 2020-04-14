dcdce03cf4442c001560b79d49a746debeb43fe2
geekQiaQia <xiyangbai@outlook.com>
2020-04-14 14:18:06 +0800

    udpate-template-component

---
+++ template/components/commentStars/commentStars.js
@@ -0,0 +1,30 @@
+// components/commentStars/commentStars.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    maxStars:{
+      type:Number,
+      value:5
+    },
+	stars:{
+		type:Number,
+		value:0
+	}
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ template/pages/index/index.js
@@ -0,0 +1,131 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+	  maxStars:5,
+	  bookInfo:{
+	  url:"https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg",
+	  star:4
+	  },
+	  cardInfoArray:[
+	  		{
+	  			title:"《蘑菇屋》",
+	  			forward:"9",
+	  			reply:"10",
+	  			like:"10058",
+	  			resume:"看书之后，震撼人心，书中对主角阿米尔的心里描写很深刻，也揭露人性的灰色地带，小时候评判世界的标准就是好人与坏人之区别，长大了与复杂社会的碰撞，每一次碰壁也慢慢了解人性这中不仅有黑白之分，还有书中对主角阿米尔的心里描写很深刻，也揭露人性的灰色地带，小时候评判世界的标准就是好人与坏人之区别，长大了与复杂社会的碰撞，",
+	  			star:5,
+	  			time:"2020-04-13",
+	  			notice:"读过",
+	  			name:"林间小鹿",
+	  			readMore:false,
+	  			iconUrl:"https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg"
+	  		},
+	  		  {
+	  		  	title:"《蘑菇屋》",
+	  		  	forward:"10",
+	  		  	reply:"10",
+	  		  	like:"10058",
+	  		  	resume:"看书之后，震撼人心，书中对主角阿米尔的心里描写很深刻，也揭露人性的灰色地带，小时候评判世界的标准就是好人与坏人之区别，长大了与复杂社会的碰撞，每一次碰壁也慢慢了解人性这中不仅有黑白之分，还有",
+	  		    star:3,
+	  			time:"2020-04-14",
+	  		  	notice:"转发",
+	  		  	name:"林间小鹿",
+	  			readMore:false,
+	  		  	iconUrl:"https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg"
+	  		  } ,
+	  		   {
+	  		   	title:"《蘑菇屋》",
+	  		   	forward:"11",
+	  		   	reply:"10",
+	  		   	like:"10058",
+	  		   	resume:"看书之后，震撼人心，书中对主角阿米尔的心里描写很深刻，也揭露人性的灰色地带，小时候评判世界的标准就是好人与坏人之区别，长大了与复杂社会的碰撞，每一次碰壁也慢慢了解人性这中不仅有黑白之分，还有",
+	  		   	star:2,
+	  			time:"2020-04-15",
+	  		   	notice:"转发",
+	  		   	name:"林间小鹿",
+	  			readMore:false,
+	  		   	iconUrl:"https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg"
+	  		   }  
+	  ],
+	  swiperList: [{
+	    id: 0,
+	    type: 'image',
+	    url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+	  }, {
+	    id: 1,
+	      type: 'image',
+	      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+	  }, {
+	    id: 2,
+	    type: 'image',
+	    url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+	  }, {
+	    id: 3,
+	    type: 'image',
+	    url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+	  }, {
+	    id: 4,
+	    type: 'image',
+	    url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+	  }, {
+	    id: 5,
+	    type: 'image',
+	    url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+	  }, {
+	    id: 6,
+	    type: 'image',
+	    url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+	  }],
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    motto: 'Hi 开发者！',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
